dfcc5a73e35938dcb143431cbd881c1a2b933f66
Richard R Panzer <ricky@ricky-HP-Pavilion-dm4-Notebook-PC.(none)>
2011-10-30 21:19:13 -0400

    tester

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThecomIt::Application.config.secret_token = '119debae3e142d317bcc317b40a406e2a527d6432d0ab27f440951c8b6eae006e201804a0b5c3148d7f023b518a8861a7b55400696fdd1464673638835077cfb'
